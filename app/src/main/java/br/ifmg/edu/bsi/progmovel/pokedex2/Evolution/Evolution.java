9e327139eb16edc622bcacd8baaae713e032ff29
Luana Assis Silva <88562681+luanaassis@users.noreply.github.com>
2023-07-15 10:31:04 -0300

    Update Evolution.java

--- app/src/main/java/br/ifmg/edu/bsi/progmovel/pokedex2/Evolution/Evolution.java
+++ app/src/main/java/br/ifmg/edu/bsi/progmovel/pokedex2/Evolution/Evolution.java
@@ -3,6 +3,6 @@ package br.ifmg.edu.bsi.progmovel.pokedex2.Evolution;
 import br.ifmg.edu.bsi.progmovel.pokedex2.Pokemon.Species;
 
 public class Evolution {
-    public Evolution evolves_to[];
+    public Evolution[] evolves_to;
     public Species species;
 }
